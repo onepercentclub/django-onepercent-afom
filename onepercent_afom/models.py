150577857ed3d2e2f1e445feef43760656768db7
Aksel Ethem <aksel.ethem@gmail.com>
2014-12-05 15:54:01 +0100

    updated for new donation flow

--- onepercent_afom/models.py
+++ onepercent_afom/models.py
@@ -4,60 +4,50 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.conf import settings
 from django.dispatch import receiver
-from apps.fund.models import Donation, DonationStatuses # Old donation model
-
-# The commented code below is a start to adapt this code to the new donation flow once its ready
-
-#from bluebottle.utils.model_dispatcher import get_donation_model
-
-# DONATION_MODEL = get_donation_model()
-
-# @receiver(pre_save, sender=DONATION_MODEL)
-# def post_to_friend_of_mine(sender, created, **kwargs):
+from bluebottle.utils.model_dispatcher import get_order_model
+from bluebottle.utils.utils import StatusDefinition
 
 
-@receiver(pre_save, weak=False, sender=Donation)
-def post_to_friend_of_mine(sender, instance, **kwargs):
-    return 
+ORDER_MODEL = get_order_model()
+
+
+@receiver(pre_save, weak=False, sender=ORDER_MODEL)
+def post_to_friend_of_mine(sender, instance, **kwargs): 
     
-    donation = instance
+    for donation in instance.donations.all():
+        #donation = instance.donation
 
-    # Only post to AFOM for one-off donations. 
-    if donation.donation_type != Donation.DonationTypes.one_off:
-        return
-
-    # If the instance has no PK the previous status is unknown.
-    if donation.pk:
-        # NOTE: We cannot check the previous and future state of the ready attribute since it is set in the
-        # Donation.save function.
-
-        existing_donation = Donation.objects.get(pk=donation.pk)
-        # If the existing donation is already pending, don't mail.
-        if existing_donation.status in [DonationStatuses.pending, DonationStatuses.paid]:
+        # Don't send to AFOM with anonymous donations
+        if donation.anonymous:
             return
 
-    # If the donation status will be pending, send a mail.
-    if donation.status in [DonationStatuses.pending, DonationStatuses.paid]:
+        # Only post to AFOM for one-off donations. 
+        if donation.order.order_type != 'one-off':
+            return
 
-        identifier = None
-        if donation.fundraiser:
-            identifier = donation.fundraiser.owner.username
-        else:
-            identifier = donation.project.owner.username
+        # If the donation status will be pending, do a request to AFOM.
+        if donation.order.status in [StatusDefinition.PENDING, StatusDefinition.SUCCESS]:
 
-        payload = {}
-        payload['identifier'] = identifier # A Friend of Mine has a list of predefined accounts
-        payload['amount'] = donation.amount
+            identifier = None
+            if donation.fundraiser:
+                identifier = donation.fundraiser.owner.username
+            else:
+                identifier = donation.project.owner.username
 
-        if donation.project:
-            payload['project'] = donation.project.title
+            payload = {}
+            payload['identifier'] = identifier # Allowed usernames are in the AFOM CMS
+            payload['amount'] = int(donation.amount) # Cast decimal to int for JSON
 
-        if donation.user:
-            payload['first_name'] = donation.user.first_name
-            payload['last_name'] = donation.user.last_name
-            payload['city'] = donation.user.location
-            payload['email'] = donation.user.email
+            if donation.project:
+                payload['project'] = donation.project.title
 
-        url = "http://stage-onepercentclub.campaignapps.nl/api/donations/new"
-                
-        res = requests.post(url, data=json.dumps(payload))
+            if donation.user:
+                payload['first_name'] = donation.order.user.first_name
+                payload['last_name'] = donation.order.user.last_name
+                payload['city'] = donation.order.user.location
+                payload['email'] = donation.order.user.email
+
+            headers = {'content-type': 'application/json'}
+            url = "http://stage-onepercentclub.campaignapps.nl/api/donations/new"
+            res = requests.post(url, data=json.dumps(payload), headers=headers)
+
