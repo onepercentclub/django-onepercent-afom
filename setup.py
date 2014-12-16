3b7e20de3c1a4681b8cce8b81c92bfc1e9b1f7e4
rollick <rollick@users.noreply.github.com>
2014-12-16 17:05:39 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name="django-onepercent-afom",
-	version='0.1',
+	version='0.2',
 	packages=['onepercent_afom'],
 	include_package_data=True,
 	license='None',
